5208b8df8467e7f9370fff7b7e2ab380270927b4
patriajasel <105503699+patriajasel@users.noreply.github.com>
2025-06-09 18:16:23 +0800

    Added initial pages for cashier, inventory, scanner, transaction, and analytics page

---
+++ lib/features/pages/cashier/cashier_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CashierPage extends StatelessWidget {
+  const CashierPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Cashier Page')));
+  }
+}
