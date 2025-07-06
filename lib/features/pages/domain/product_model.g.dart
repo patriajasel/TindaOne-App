d0081d5ea603f3f50ce4b86ea69120d3d2d21427
patriajasel <105503699+patriajasel@users.noreply.github.com>
2025-07-06 09:23:08 +0800

    Hive Setup Update
    // Redirected different pages to presentaion folder
    // Added Product Model

---
+++ lib/features/pages/domain/product_model.g.dart
@@ -0,0 +1,159 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductModelHiveAdapter extends TypeAdapter<ProductModelHive> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ProductModelHive read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProductModelHive()
+      ..productId = fields[0] as String
+      ..name = fields[1] as String
+      ..image = fields[2] as String
+      ..isInclusion = fields[3] as bool
+      ..priceType = fields[4] as String
+      ..productSizes = fields[5] as ProductSizesHive?
+      ..price = fields[6] as int?
+      ..supply = fields[7] as int?
+      ..size = fields[8] as String?
+      ..barCode = fields[9] as String?;
+  }
+
+  @override
+  void write(BinaryWriter writer, ProductModelHive obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.productId)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.image)
+      ..writeByte(3)
+      ..write(obj.isInclusion)
+      ..writeByte(4)
+      ..write(obj.priceType)
+      ..writeByte(5)
+      ..write(obj.productSizes)
+      ..writeByte(6)
+      ..write(obj.price)
+      ..writeByte(7)
+      ..write(obj.supply)
+      ..writeByte(8)
+      ..write(obj.size)
+      ..writeByte(9)
+      ..write(obj.barCode);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductModelHiveAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ProductSizesHiveAdapter extends TypeAdapter<ProductSizesHive> {
+  @override
+  final int typeId = 2;
+
+  @override
+  ProductSizesHive read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProductSizesHive()
+      ..size = fields[0] as String
+      ..price = fields[1] as int
+      ..supply = fields[2] as int
+      ..barCode = fields[3] as String?;
+  }
+
+  @override
+  void write(BinaryWriter writer, ProductSizesHive obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.size)
+      ..writeByte(1)
+      ..write(obj.price)
+      ..writeByte(2)
+      ..write(obj.supply)
+      ..writeByte(3)
+      ..write(obj.barCode);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductSizesHiveAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ProductModelImpl _$$ProductModelImplFromJson(Map<String, dynamic> json) =>
+    _$ProductModelImpl(
+      productId: json['productId'] as String,
+      name: json['name'] as String,
+      image: json['image'] as String,
+      isInclusion: json['isInclusion'] as bool,
+      priceType: json['priceType'] as String,
+      productSizes: json['productSizes'] == null
+          ? null
+          : ProductSizes.fromJson(json['productSizes'] as Map<String, dynamic>),
+      price: (json['price'] as num?)?.toInt() ?? null,
+      supply: (json['supply'] as num?)?.toInt() ?? null,
+      size: json['size'] as String? ?? null,
+      barCode: json['barCode'] as String? ?? null,
+    );
+
+Map<String, dynamic> _$$ProductModelImplToJson(_$ProductModelImpl instance) =>
+    <String, dynamic>{
+      'productId': instance.productId,
+      'name': instance.name,
+      'image': instance.image,
+      'isInclusion': instance.isInclusion,
+      'priceType': instance.priceType,
+      'productSizes': instance.productSizes,
+      'price': instance.price,
+      'supply': instance.supply,
+      'size': instance.size,
+      'barCode': instance.barCode,
+    };
+
+_$ProductSizesImpl _$$ProductSizesImplFromJson(Map<String, dynamic> json) =>
+    _$ProductSizesImpl(
+      size: json['size'] as String,
+      price: (json['price'] as num).toInt(),
+      supply: (json['supply'] as num).toInt(),
+      barCode: json['barCode'] as String? ?? null,
+    );
+
+Map<String, dynamic> _$$ProductSizesImplToJson(_$ProductSizesImpl instance) =>
+    <String, dynamic>{
+      'size': instance.size,
+      'price': instance.price,
+      'supply': instance.supply,
+      'barCode': instance.barCode,
+    };
---
+++ lib/features/pages/presentation/cashier/cashier_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:searchfield/searchfield.dart';
+import 'package:tinda_one_app/features/pages/presentation/cashier/add_item_dialog/add_item_dialog.dart';
+import 'package:tinda_one_app/features/pages/presentation/cashier/create_order/create_order_dialog.dart';
+import 'package:tinda_one_app/shared/common/item_card.dart';
+
+class CashierPage extends StatelessWidget {
+  const CashierPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final suggestions = [
+      SearchFieldListItem('Blue Blouse'),
+      SearchFieldListItem('Red Shirt'),
+      SearchFieldListItem('Green Skirt'),
+      SearchFieldListItem('Yellow Pants'),
+      SearchFieldListItem('Black Jacket'),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cashier'),
+        actions: [_buildCartButton(context)],
+      ),
+      body: _buildMainContent(context, suggestions),
+    );
+  }
+
+  Widget _buildMainContent(
+    BuildContext context,
+    List<SearchFieldListItem<dynamic>> suggestions,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Section Title
+          _buildSectionTitle(context),
+
+          const SizedBox(height: 10),
+          Divider(color: Theme.of(context).dividerColor, thickness: 1.5),
+
+          // Search Field
+          const SizedBox(height: 16),
+          _buildSearchField(suggestions),
+          const SizedBox(height: 16),
+
+          // Product List
+          Expanded(child: _buildProductList()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return ItemCard(
+          productId: '1',
+          name: 'Blue Blouse',
+          imageUrl: 'https://picsum.photos/200/300',
+          sizes: ['S', 'M', 'L', '16', '18', '20'],
+          onPressed: () => showModalBottomSheet(
+            context: context,
+            builder: (context) => AddItemDialog(
+              productId: '1',
+              name: 'Blue Blouse',
+              sizes: [
+                '8',
+                '10',
+                '12',
+                '14',
+                '16',
+                '18',
+                '20',
+                '22',
+                '24',
+                'XS',
+                'S',
+                'M',
+                'L',
+                'XL',
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSearchField(List<SearchFieldListItem<dynamic>> suggestions) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: SearchField(
+        suggestions: suggestions,
+        suggestionState: Suggestion.expand,
+        maxSuggestionsInViewPort: 5,
+        itemHeight: 50,
+        searchInputDecoration: SearchInputDecoration(
+          hintText: 'Search products...',
+          prefixIcon: const Icon(Icons.search),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.inventory_2_outlined,
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+        const SizedBox(width: 10),
+        Text(
+          'Product Catalog',
+          style: Theme.of(
+            context,
+          ).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCartButton(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary.withValues(alpha: 0.3),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart_outlined),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => CreateOrderDialog(),
+              );
+            },
+          ),
+          Positioned(
+            top: 6,
+            right: 6,
+            child: Container(
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              child: const Text(
+                '3',
+                style: TextStyle(fontSize: 12, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
